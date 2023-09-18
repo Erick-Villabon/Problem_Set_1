308d83043dbf6c9d2b460806031e410bf3988a6a
Erick-Villabon <142061036+Erick-Villabon@users.noreply.github.com>
2023-09-18 15:30:11 -0500

    Update punto 4.R

--- scripts/punto 4.R
+++ scripts/punto 4.R
@@ -3,163 +3,23 @@
 #                      Problem Set 1: Predicting Income                       #
 #                                                                             #
 #-----------------------------------------------------------------------------#
-#   Autores: -                                                    
-#            -   
-#            - 
-#            - 
+#   Autores: -  Erick Julian Villabon - 201815677                                                  
+#            -  Juan Diego Duarte - 202011999  
+#            -  Carlos Torres Sandoval - 202225155
+#            -  Tania Reina - 202015300 
 #
 #  Fecha: 18/09/2023 
 #
-# Punto 4
+#Punto 4
 
-rm(list = ls())
-cat("\014")
-
-# Librerias
-require(pacman)
-p_load(tidyverse, skimr, stargazer, tidymodels, broom, knitr, kableExtra, zoo, 
-       boot, RCurl, gdata, lmtest, xtable, sjPlot, writexl, ggplot2, openxlsx, rvest)
-
-
-# - 1. Importar Datos
-
-GEIH <-  data.frame()
-
-for (j in 1:10) {
-  url <- "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_"
-  pagina <- paste0(url,j,".html")
-  Data_GitHub <- read_html(pagina)
-  temp <-  Data_GitHub %>%
-    html_table()
-  GEIH <- bind_rows(GEIH,temp)
-}
-
-# 2. Limpieza de la base de datos
-# Edad -Sólo mayores de 18 años-
-
-GEIH <- rename(GEIH, c("edad" = "age"))
-GEIH <- GEIH[GEIH$edad >= 18, ]
-
-# Años de educación
-
-GEIH$edad_2 <- GEIH$edad^2
-
-# Ingresos:
-
-#Total
-
-GEIH <- rename(GEIH, c("ingreso_total" = "ingtot"))
-
-#Mensual
-
-GEIH <- rename(GEIH, c("salario_mensual" = "y_ingLab_m"))
-
-# Género
-
-GEIH$mujer <- ifelse(GEIH$sex == 0, 1, 0)
-GEIH$mujer[GEIH$sex == 1] <- 0
-
-# Estudiante
-
-GEIH$estudiante <- ifelse(GEIH$p6240 == 3, 1, 0)
-GEIH$estudiante[GEIH$p6240 != 3] <- 0
-GEIH$estudiante[GEIH$p6240 == "."] <- NA
-
-# Busca trabajo
-
-GEIH$busca_trabajo <- ifelse(GEIH$p6240 == 2, 1, 0)
-GEIH$busca_trabajo[GEIH$p6240 != 2] <- 0
-GEIH$busca_trabajo[GEIH$p6240 == "."] <- NA
-
-# Amo(a) de casa
-
-GEIH$amo_casa <- ifelse(GEIH$p6240 == 4, 1, 0)
-GEIH$amo_casa[GEIH$p6240 != 4] <- 0
-GEIH$amo_casa[GEIH$p6240 == "."] <- NA
-
-# Hijos en el hogar
-
-GEIH$hijos_hogar <- ifelse(GEIH$p6050 == 3, 1, 0)
-GEIH$hijos_hogar[GEIH$p6050 != 3] <- 0
-GEIH$hijos_hogar[GEIH$p6050 == "."] <- NA
-
-# Primaria
-
-GEIH$primaria <- ifelse(GEIH$p6210 == 1, 1, 0)
-GEIH$primaria[GEIH$p6210 == "."] <- NA
-
-# Secundaria
-
-GEIH$secundaria <- ifelse(GEIH$p6210 == 4, 1, 0)
-GEIH$secundaria[GEIH$p6210 == "."] <- NA
-
-# Media
-
-GEIH$media <- ifelse(GEIH$p6210 == 5, 1, 0)
-GEIH$media[GEIH$p6210 == "."] <- NA
-
-# Superior
-
-GEIH$superior <- ifelse(GEIH$p6210 == 6, 1, 0)
-GEIH$superior[GEIH$p6210 == "."] <- NA
-
-# Mantener Ocupados
-
-GEIH <- GEIH[GEIH$oc == 1, ]
-
-
-# Experiencia trabajo actual
-
-GEIH <- rename(GEIH, c("exp_trab_actual" = "p6426"))
-
-# Estrato
-
-GEIH <- rename(GEIH, c("estrato" = "estrato1"))
-
-# Cabecera
-
-GEIH$cabecera <- ifelse(GEIH$clase == 1, 1, 0)
-
-# Horas de trabajo a la semana
-
-GEIH <- rename(GEIH, c("horas_trab_usual" = "hoursWorkUsual"))
-
-# Logaritmo del Salario
-
-GEIH$log_salario_m <- log(GEIH$salario_mensual)
-
-# Ciudad
-
-GEIH <- rename(GEIH, c("ciudad" = "dominio"))
-
-
-# Mantener variables
-
-GEIH <- subset(GEIH, select = c("directorio", "secuencia_p", "orden",
-                                "mes", "edad", "edad_2", "mujer", 
-                                "estudiante", "busca_trabajo", "amo_casa",
-                                "hijos_hogar", "primaria", "secundaria",
-                                "media", "superior", "salario_mensual",
-                                "ingreso_total", "exp_trab_actual",
-                                "estrato", "cabecera", "horas_trab_usual",
-                                "oficio", "log_salario_m", "informal",
-                                "ciudad"))
-
-# Limpieza de valores faltantes 
-
-# Se eliminan las observaciones que tienen valores faltantes en el
-# salario nominal mensual
-
-GEIH <- GEIH %>% filter(!is.na(salario_mensual))
-
-###############################################################################
-#CUARTO PUNTO##
 #Regresión
+
 mod3 <- lm(log_salario_m ~ mujer, data = GEIH, x= TRUE)
 stargazer(mod3, type = "text", omit.stat=c("ser","f","adj.rsq"), out = "C:/Users/carlo/Documents/Out/Regresion 4.1.html")
 
 # Ahora vamos a usar FWL usando como variables condicionales: experiencia de trabajo actual,
 # horas de trabajo usual, oficio y edad.
+
 GEIH2 <- GEIH
 
 mod4 <- lm(log_salario_m~mujer + exp_trab_actual + horas_trab_usual + oficio + edad, data = GEIH2)
@@ -178,9 +38,7 @@ stargazer(mod3, mod4, modFWL, type = "text", omit.stat=c("ser","f","adj.rsq"), o
 stargazer(mod3, mod4, modFWL, type = "html", omit.stat=c("ser","f","adj.rsq"))
 
 
-
-
-####################################################################################
+################################################################################
 
 #Bootstrap FWL
 
